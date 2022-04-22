b66c8dcb23362d6e0cd606a8ba0d3cc2cc9f2e67
kankshicodes <raviald23@gmail.com>
2022-04-22 12:21:48 +0530

    files

---
+++ classes/Project.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class Project
+{
+private:
+    /* data */
+public:
+    Project(/* args */);
+    ~Project();
+};
+
+Project::Project(/* args */)
+{
+}
+
+Project::~Project()
+{
+}
+
+class Task{
+
+
+};
+
+
+int main(){
+    return 0;
+}
